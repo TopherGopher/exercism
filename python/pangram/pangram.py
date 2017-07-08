ee7ad6b536bfb490c56bc769082ba1ebbf108404
Topher Sterling <topher@develops.guru>
2017-07-08 13:47:15 -0600

    Pangram solution solved.

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,15 @@
-def is_pangram():
-    pass
+from string import ascii_lowercase
+
+def is_pangram(sentence):
+    tmp_list = {}
+    for c in sentence:
+      if c.lower() in ascii_lowercase:
+        if c not in tmp_list.keys():
+          tmp_list[c] = 1
+        else:
+          tmp_list[c] += 1
+    if len(tmp_list) == 26:
+      return True
+    else:
+      return False
+
